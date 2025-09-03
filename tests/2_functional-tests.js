d786a9268dbb4319fd16b068a87c10f9c1474745
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-09-03 11:54:24 +0200

    GET request to /api/stock-prices

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,30 +7,15 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
-  test('Viewing two stocks and liking them: GET request to /api/stock-prices/', function(done) {
+  test('Viewing one stock: GET request to /api/stock-prices', function(done) {
     chai.request(server)
-      .get('/api/stock-prices/')
-      .query({ stock: ['GOOG', 'AAPL'], like: true }) // two stocks with like
+      .get('/api/stock-prices')
+      .query({ stock: 'GOOG' }) 
       .end(function(err, res) {
-        assert.equal(res.status, 200);
-        assert.property(res.body, 'stockData');
-        assert.isArray(res.body.stockData, 'stockData should be an array');
-        assert.lengthOf(res.body.stockData, 2);
-
-        // First stock
-        assert.equal(res.body.stockData[0].stock, 'GOOG');
-        assert.property(res.body.stockData[0], 'price');
-        assert.property(res.body.stockData[0], 'rel_likes'); // relative likes
-
-        // Second stock
-        assert.equal(res.body.stockData[1].stock, 'AAPL');
-        assert.property(res.body.stockData[1], 'price');
-        assert.property(res.body.stockData[1], 'rel_likes'); // relative likes
-
-        // Optional: relative likes logic check
-        const relLikesSum = res.body.stockData[0].rel_likes + res.body.stockData[1].rel_likes;
-        assert.equal(relLikesSum, 0, 'Relative likes should sum to 0');
-
+        assert.equal(res.status, 200);          
+        assert.property(res.body, 'stockData');  
+        assert.equal(res.body.stockData.stock, 'GOOG'); 
+        assert.property(res.body.stockData, 'price'); 
         done();
       });
   });
