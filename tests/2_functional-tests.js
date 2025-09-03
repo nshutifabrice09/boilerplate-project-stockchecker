e3bd5f20fcc12a82aca1ee91bef1f7bc3a356aa8
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-09-03 23:04:13 +0200

    newTest stockData commit

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,16 +7,27 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
-  test('GET /api/stock-prices with stock query parameter returns stockData', function(done) {
+  test('stockData includes stock (string), price (number), and likes (number)', function(done) {
     chai.request(server)
         .get('/api/stock-prices')
-        .query({ stock: 'AAPL' })
+        .query({ stock: 'GOOG' })
         .end(function(err, res) {
-        assert.equal(res.status, 200);
-        assert.property(res.body, 'stockData');
-        assert.isObject(res.body.stockData);
-        done();
+            assert.equal(res.status, 200);
+            assert.property(res.body, 'stockData');
+
+            const stockData = res.body.stockData;
+
+            assert.property(stockData, 'stock');
+            assert.isString(stockData.stock, 'Stock symbol should be a string');
+
+            assert.property(stockData, 'price');
+            assert.isNumber(stockData.price, 'Price should be a number');
+        
+            assert.property(stockData, 'likes');
+            assert.isNumber(stockData.likes, 'Likes should be a number');
+        
+            done();
+        });
     });
-});
 
 });
