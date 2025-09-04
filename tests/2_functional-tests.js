8d66711fb7b980d5e8270c2631e836ba59875456
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-09-04 09:56:04 +0200

    content security policies

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,55 +7,18 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
-  test('stockData includes stock (string), price (number), and likes (number)', function(done) {
+  test('Viewing one stock: GET request to /api/stock-prices/', function(done) {
     chai.request(server)
-        .get('/api/stock-prices')
-        .query({ stock: 'GOOG' })
-        .end(function(err, res) {
-            assert.equal(res.status, 200);
-            assert.property(res.body, 'stockData');
-
-            const stockData = res.body.stockData;
-
-            assert.property(stockData, 'stock');
-            assert.isString(stockData.stock, 'Stock symbol should be a string');
-
-            assert.property(stockData, 'price');
-            assert.isNumber(stockData.price, 'Price should be a number');
-        
-            assert.property(stockData, 'likes');
-            assert.isNumber(stockData.likes, 'Likes should be a number');
-        
-            done();
-        });
-    });
-
-    test('Liking a stock adds 1 like', function(done) {
-        chai.request(server)
-        .get('/api/stock-prices')
-        .query({ stock: 'GOOG', like: true })
-        .end(function(err, res) {
-            assert.equal(res.status, 200);
-            const stockData = res.body.stockData;
-            assert.property(stockData, 'likes');
-            assert.isNumber(stockData.likes);
-            assert.isAtLeast(stockData.likes, 1);
-            done();
-        });
-    });
-
-    test('Liking same stock again from same IP should not increase likes', function(done) {
-        chai.request(server)
-        .get('/api/stock-prices')
-        .query({ stock: 'GOOG', like: true })
-        .end(function(err, res) {
-            assert.equal(res.status, 200);
-            const stockData = res.body.stockData;
-            assert.property(stockData, 'likes');
-            assert.isNumber(stockData.likes);
-        // This test just ensures that like is not increased again
-            done();
-        });
-    });
+      .get('/api/stock-prices/')
+      .query({ stock: 'GOOG' }) // replace 'GOOG' with the stock symbol you want to test
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData'); // make sure the response has stockData
+        assert.equal(res.body.stockData.stock, 'GOOG'); // check the stock symbol
+        assert.property(res.body.stockData, 'price'); // check that price exists
+        done();
+      });
+  });
 
 });
+
